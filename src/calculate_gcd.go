16096f4fca0ec765f3da95e3f63a40dbe6f1eb7b
Muhammad Shamsul Maruf <smaruf00ruet320@gmail.com>
2019-11-22 16:05:22 +0600

    fix syntax 2019.11.22.1605

--- src/calculate_gcd.go
+++ src/calculate_gcd.go
@@ -14,10 +14,10 @@ func (numbers *GCD) calculateGcd() int64 {
     }
 }
 
-private function (numbers *GCD) getMin() int64 {
+private func (numbers *GCD) getMin() int64 {
     return numbers.first < numbers.second ? numbers.first : numbers.second
 }
 
-private function (numbers *GCD) getMax() int64 {
+private func (numbers *GCD) getMax() int64 {
     return numbers.first > numbers.second ? numbers.first : numbers.second
 }
