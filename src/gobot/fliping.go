8bd1546024f34fc2bb8c790fd18d6648418cc50a
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2022-08-04 13:32:07 +0600

    Flip DJI 220804-1332

---
+++ src/gobot/fliping.go
@@ -0,0 +1,36 @@
+package main
+
+  import (
+      "time"
+
+      "gobot.io/x/gobot"
+      "gobot.io/x/gobot/platforms/dji/tello"
+  )
+
+  func main() {
+      drone := tello.NewDriver("8888")
+
+      work := func() {
+          drone.TakeOff()
+
+          gobot.After(5*time.Second, func() {
+              drone.FrontFlip()
+          })
+
+          gobot.After(10*time.Second, func() {
+              drone.BackFlip()
+          })
+
+          gobot.After(15*time.Second, func() {
+              drone.Land()
+          })
+      }
+
+      robot := gobot.NewRobot("tello",
+          []gobot.Connection{},
+          []gobot.Device{drone},
+          work,
+      )
+
+      robot.Start()
+  }
