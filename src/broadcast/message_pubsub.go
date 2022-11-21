969b8dee0a67d069a5d256f263af2119a6d4a7ed
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2022-11-21 11:37:23 +0600

    Sample message pubsub 221121-1135

---
+++ src/broadcast/message_pubsub.go
@@ -0,0 +1,23 @@
+package broadcast
+
+// Message The message metadata
+type Message struct {
+	topic     string
+	payload   interface{}
+	createdAt int64
+}
+
+// GetTopic Return the topic of the current message
+func (m *Message) GetTopic() string {
+	return m.topic
+}
+
+// GetPayload Get the payload of the current message
+func (m *Message) GetPayload() interface{} {
+	return m.payload
+}
+
+// GetCreatedAt Get the creation time of this message
+func (m *Message) GetCreatedAt() int64 {
+	return m.createdAt
+}
