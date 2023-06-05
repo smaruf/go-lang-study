fe7104a1e19a4ee61151b8ac22b0f2a9840e58b4
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2023-06-05 14:10:24 +0200

    Create hello.go 230631-1409

---
+++ src/aws/docker/hello.go
@@ -0,0 +1,15 @@
+package main
+import (
+    "context"
+    "fmt"
+    "github.com/aws/aws-lambda-go/lambda"
+)
+type MyEvent struct {
+    Name string `json:"name"`
+}
+func HandleRequest(_ context.Context, name MyEvent) (string, error) {
+    return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+func main() {
+    lambda.Start(HandleRequest)
+}
